927b809b03e176879cc0b2e1a808795f39c5c7a8
shenao <shenao@wepie.com>
2021-06-30 16:19:05 +0800

    chapter 9 exercise

--- chapter9.js
+++ chapter9.js
@@ -31,3 +31,64 @@ fullname=Davaeorn
 type=evil wizard
 outputdir=/home/marijn/enemies/davaeorn
 `))
+
+// Regexp golf
+
+verify(/ca[rt]/,
+  ["my car", "bad cats"],
+  ["camper", "high art"])
+
+verify(/pr?op/,
+  ["pop culture", "mad props"],
+  ["plop", "prrrop"])
+
+verify(/ferr(et|y|ari)/,
+  ["ferret", "ferry", "ferrari"],
+  ["ferrum", "transfer A"])
+
+verify(/ious\b/,
+  ["how delicious", "spacious room"],
+  ["ruinous", "consciousness"])
+
+verify(/\s[,.:;]/,
+  ["bad punctuation ."],
+  ["escape the period"])
+
+verify(/\w{7}/,
+  ["Siebentausenddreihundertzweiundzwanzig"],
+  ["no", "three small words"])
+
+verify(/\b[^\We]+\b/i,
+  ["red platypus", "wobbling nest"],
+  ["earth bed", "learnig ape", "BEET"])
+
+
+function verify(regexp, yes, no) {
+  for (const str of yes) if (!regexp.test(str)) {
+    console.log(`Failure to match '${str}'`)
+  }
+  for (const str of no) if (regexp.test(str)) {
+    console.log(`Unexpected match for '${str}'`)
+  }
+}
+
+// Quoting style
+
+const text = "'I'm the cook,' he said, 'it's my job.'"
+console.log(text.replace(/(^|\W)'|'($|\W)/g, '$1"$2'))
+// → "I'm the cook," he said, "it's my job."
+
+// Numbers again
+
+const number = /^[-+]?(\d+(\.\d*)?|\.\d+)(e[-+]?\d+)?$/i
+
+for (const str of ["1", "-1", "+15", "1.55", ".5", "5.", "1.3e2", "1E-4", "1e+12"]) {
+  if (!number.test(str)) {
+    console.log(`Failed to match '${str}'`)
+  }
+}
+for (const str of ["1a", "+-1", "1.2.3", "1+1", "1e4.5", ".5.", "1f5", "."]) {
+  if (number.test(str)) {
+    console.log(`Incorrectly accepted '${str}'`)
+  }
+}
