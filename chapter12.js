3b16603c0b37f7d05a989e4abbd8f5fbd6422172
shenao <shenao@wepie.com>
2021-07-22 20:56:45 +0800

    chapter 12 exercise

--- chapter12.js
+++ chapter12.js
@@ -159,3 +159,65 @@ specialForms.fun = (args, scope) => {
     return evaluate(body, localScope)
   }
 }
+
+// Arrays
+
+topScope.array = (...values) => values
+
+topScope.length = array => array.length
+
+topScope.element = (array, i) => array[i]
+
+run(`
+do(define(sum, fun(array,
+     do(define(i, 0),
+        define(sum, 0),
+        while(<(i, length(array)),
+          do(define(sum, +(sum, element(array, i))),
+             define(i, +(i, 1)))),
+        sum))),
+   print(sum(array(1, 2, 3))))
+`)
+// → 6
+
+// Comments
+
+function skipSpace(string) {
+  const skippable = string.match(/^(\s|#.*)*/)
+  return string.slice(skippable[0].length)
+}
+
+console.log(parse("# hello\nx"))
+// → {type: "word", name: "x"}
+
+console.log(parse("a # one\n   # two\n()"))
+// → {type: "apply",
+//    operator: {type: "word", name: "a"},
+//    args: []}
+
+// Fixing scope
+
+specialForms.set = (args, env) => {
+  if (args.length !== 2 || args[0].type !== 'word') {
+    throw new SyntaxError('Bad use of set')
+  }
+
+  const varName = args[0].name
+  const value = evaluate(args[1], env)
+  for (let scope = env; scope; scope = Object.getPrototypeOf(scope)) {
+    if (Object.prototype.hasOwnProperty.call(scope, varName)) {
+      return scope[varName] = value
+    }
+  }
+  throw new ReferenceError(`Setting undefined variable ${varName}`)
+}
+
+run(`
+do(define(x, 4),
+   define(setx, fun(val, set(x, val))),
+   setx(50),
+   print(x))
+`)
+// → 50
+run(`set(quux, true)`)
+// → Some kind of ReferenceError
